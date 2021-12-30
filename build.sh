c3ad2e8ab9d5f33c6733441d8814e773783b9a7c
konstare <60809944+konstare@users.noreply.github.com>
2021-12-30 23:28:53 +0300

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env sh
-docker build -t konstare/emacs-gcc-pgtk .
+docker build -t konstare/emacs-gcc-pgtk:local .
 id=$(docker create konstare/emacs-gcc-pgtk)
 docker cp $id:/opt/deploy .
